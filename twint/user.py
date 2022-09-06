bc7d3e55b102d3967eb4ca30edda115fbeeb3e47
Leo Meyerovich <leo@graphistry.com>
2022-09-05 19:14:13 -0700

    fix(user): fix user checker

--- twint/user.py
+++ twint/user.py
@@ -18,7 +18,7 @@ User_formats = {
 # ur object must be a json from the endpoint https://api.twitter.com/graphql
 def User(ur):
     logme.debug(__name__ + ':User')
-    if 'data' not in ur and 'user' not in ur['data']:
+    if 'data' not in ur or 'user' not in ur['data']:
         msg = 'malformed json! cannot be parsed to get user data'
         logme.fatal(msg)
         raise KeyError(msg)
